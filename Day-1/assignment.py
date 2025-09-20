38db84cb52a24e838bd2f9660b914f9200994cb5
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-20 23:20:15 +0530

    Update assignment.py

--- Day-1/assignment.py
+++ Day-1/assignment.py
@@ -1,17 +1,17 @@
 print("welcome to band name generator")
 
-name_of_city=input("What's the name of the city you grew up in")
+name_of_city=input("What's the name of the city you grew up in?\n")
 
-name_of_pet=input("whats your pet name\n")
+name_of_pet=input("whats your pet name?\n")
 
 
 print("your band name could be " + name_of_city + " " + name_of_pet)
-
 ----------
 o/p
-What's the name of the city you grew up injamshedpur
-whats your pet name
-ranjan
-your band name could be jamshedpur ranjan
+What's the name of the city you grew up in?
+jamshedur
+whats your pet name?
+blacky
+your band name could be jamshedpur blacky
 
 Process finished with exit code 0
