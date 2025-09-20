994a36e0531204fc4b067847627ce347c0fecba0
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-20 17:42:02 +0530

    Create variables.py

---
+++ Day-1/variables.py
@@ -0,0 +1,11 @@
+name="Ranjan"
+
+print(name)
+
+name="Yadav"
+
+print(name)
+
+o/p-> C:\Users\Lenovo\AppData\Local\Programs\Python\Python313\python.exe "C:\Users\Lenovo\Downloads\marketplace-100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp-9\contents\Day 1\Variables\task.py" 
+Ranjan
+Yadav
