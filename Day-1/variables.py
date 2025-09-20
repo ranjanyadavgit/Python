7590820988f24a4ea34764216ad14b88aa960d84
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-20 18:01:15 +0530

    Update variables.py

--- Day-1/variables.py
+++ Day-1/variables.py
@@ -9,3 +9,14 @@ print(name)
 o/p-> C:\Users\Lenovo\AppData\Local\Programs\Python\Python313\python.exe "C:\Users\Lenovo\Downloads\marketplace-100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp-9\contents\Day 1\Variables\task.py" 
 Ranjan
 Yadav
+----------------------------------------
+
+using len() to find the length of a string
+
+#print(len(input("what is name?")))
+username=input("what is your name?")
+string_length=len(username)
+print(string_length)
+
+what is your name?ranjan
+6
