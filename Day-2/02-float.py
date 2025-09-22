8850ed61d03ed93726408961532003e377d9d427
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-22 14:42:32 +0530

    Create 02-float.py

---
+++ Day-2/02-float.py
@@ -0,0 +1,30 @@
+#float variable
+num1 = 5.0
+num2 = 2.5
+
+result1 = num1 + num2
+
+print("addition:", result1)
+
+result2 = num1 - num2
+
+print("substraction:", result2)
+
+result3 = num1 * num2
+
+print("multiplication:", result3)
+
+result4 = num1 / num2
+
+print("divison:", result4)
+
+result5 = round(3.14235949,2)
+
+print("round:", result5)
+
+-------
+addition: 7.5
+substraction: 2.5
+multiplication: 12.5
+divison: 2.0
+round: 3.14
