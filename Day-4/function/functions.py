9f0fa11f75d6cd8bd60f8b4b9e762cd6ac99c262
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-27 22:42:04 +0530

    Create functions.py

---
+++ Day-4/function/functions.py
@@ -0,0 +1,14 @@
+import math
+
+result = math.sqrt(16)
+
+print(result)
+
+from math import pi
+
+print(pi)
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /c/Python/Day-4
+$ python calculator.py 
+4.0
+3.141592653589793
