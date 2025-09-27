50db165d54d9dd618cb2bfe9f5e7407bbc347c2c
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-27 14:51:18 +0530

    Create calculator.py

---
+++ Day-4/calculator.py
@@ -0,0 +1,25 @@
+num1 = 5
+num2 = 10
+
+def add():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    subs = num1 - num2
+    print(subs)  
+
+def mul():
+    multi = num1 * num2
+    print(multi)
+
+
+add()
+sub()
+mul()
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /c/Python/Day-4
+$ python calculator.py 
+15
+-5
+50
