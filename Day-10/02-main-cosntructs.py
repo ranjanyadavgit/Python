a825d886008a831e5bd42da158ce4419fc390966
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-10-07 11:49:17 +0530

    Create 02-main-cosntructs.py

---
+++ Day-10/02-main-cosntructs.py
@@ -0,0 +1,14 @@
+def main():
+    folder_paths=input("Enter a folder path that needs space:").split()
+    print(folder_paths)
+
+
+
+if __name__=="__main()":
+    main()
+
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /c/Python/day-10
+$ python main.py 
+enter a list of folder path that needs spaceranjan kumar yadav
+['ranjan', 'kumar', 'yadav']
