1c4d6711eba2691052b8ad1751cc7032d244d509
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-10-07 09:21:17 +0530

    Create 01-convert-string-to-list.py

---
+++ Day-10/01-convert-string-to-list.py
@@ -0,0 +1,8 @@
+folder_paths=input("Enter a list of folder paths that separated by spaces:").split()
+print(folder_paths)
+
+
+
+$ python main.py 
+Enter a list of folder paths that separated by spaces:ranjan kumar yadav
+['ranjan', 'kumar', 'yadav']
