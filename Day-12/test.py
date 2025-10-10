12ffaeb5f676460e886c64ae260d6199fd6164a5
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-10-11 00:22:50 +0530

    Create test.py

---
+++ Day-12/test.py
@@ -0,0 +1,18 @@
+def update_server_config(file_path,key,value):
+
+  #READ the existing content of the server existing file
+  with open("file_path","r") as file:
+    lines=files.readlines()
+
+ #update the configuration value for the specified key
+with open("file_path","w") as file:
+   for line in lines:
+     #check if the line starts with the specified key
+     if key in line
+      file.write(key + "=" + value + "\n")
+     else
+      # keep the file as it is
+      file.write(line)
+
+ update_server_config(server_config_file, key_to_update,new_value)
+    
