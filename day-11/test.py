480e042ba5d894226cf9d3182cf990eb929d712f
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-10-10 13:04:01 +0530

    Create test.py

---
+++ day-11/test.py
@@ -0,0 +1,13 @@
+students_properties= {
+    "name": "Ranjan",
+    "age": 15,
+    "class":10
+}
+
+print(students_properties["name"])
+print(students_properties["age"])
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /c/Python/day-11
+$ python test.py 
+Ranjan
+15
